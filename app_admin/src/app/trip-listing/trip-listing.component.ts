42014960e1e43bdf613d6200b3cbf37cb6187549
jessicamcalum <jacastil@hawaii.edu>
2024-04-17 17:43:08 -0700

    Module 6 completed baseline - Created SPA

---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TripCardComponent } from '../trip-card/trip-card.component';
+
+import { TripDataService} from '../services/trip-data.service'; // Imports the TripDataService
+import { Trip } from '../models/trip'; // Imports the Trip model
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-trip-listing',
+  standalone: true,
+  imports: [CommonModule, TripCardComponent],
+  templateUrl: './trip-listing.component.html',
+  styleUrl: './trip-listing.component.css',
+  providers: [TripDataService]  // Registers TripDataService as a provider
+})
+
+export class TripListingComponent implements OnInit {
+  
+  trips!: Trip[];
+  message: string = '';
+
+  // Constructor to initialize the TripDataService
+
+  constructor(
+    private tripDataService: TripDataService,
+    private router: Router
+    ) {
+    console.log('trip-listing constructor');
+  }
+
+  // addTrip() method that will support the new button
+
+  public addTrip(): void {
+    this.router.navigate(['add-trip']);
+  }
+
+  // Method that will call the getTrips() method in TripDataService
+  
+  private getStuff(): void {
+    this.tripDataService.getTrips()
+      .subscribe({
+        next: (value: any) => {
+          this.trips = value;
+          if(value.length > 0)
+          {
+            this.message = 'There are ' + value.length + ' trips available.';
+          }
+          else{
+            this.message = 'There were no trips retrieved from the database';
+          }
+          console.log(this.message);
+        },
+        error: (error:any) => {
+          console.log('Error: ' + error);
+        }
+      })
+  }
+
+  // ngOnInit method that will call the private method when the component is initialized
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.getStuff();
+  }
+
+}
